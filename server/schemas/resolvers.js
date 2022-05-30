9367b0c6c6dbfc4e6995d5eeecd476a2502c76ab
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-05-29 22:08:28 -0700

    graphQL queries working

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,34 @@
+//IMPORT
+const { User, Thought, Reaction } = require('../models');
+
+const resolvers = {
+    Query: {
+        //ALL
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        //ONE BY ID
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+        //ALL USERS
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        //USER BY USERNAME
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+
+    }
+
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,39 @@
 //IMPORT
 const express = require('express');
+//APPOLO IMPORT
+const { ApolloServer } = require('apollo-server-express');
+//IMPORT TYPEDEFS AND RESOLVERS
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+//NEW APOLLO SERVER
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//NEW INSTANCE OF AN APOLLO SERVER W GRAPHQL CHEMA
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  //MIDDLEWARE
+  server.applyMiddleware({ app })
+};
+
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    //TESTING GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+
 });
+
+//CALL ASYNC TO START THE SERVER
+startApolloServer(typeDefs, resolvers);
