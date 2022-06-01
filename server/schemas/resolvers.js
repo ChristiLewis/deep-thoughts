1873dcef1822ffe367fc89557f820ac73ce60e53
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-06-01 11:08:13 -0700

    update check

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,20 @@
 //IMPORT
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 const { User, Thought, Reaction } = require('../models');
 
 const resolvers = {
     Query: {
+        me: async (parent, args) => {
+            const userData = await User.findOne({})
+                .select('-__v -password')
+                .populate('thoughts')
+                .populate('friends');
+
+            return userData;
+        },
+
+        
         //ALL
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
@@ -32,11 +44,25 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            const token = signToken(user);
 
-            return user;
+            return { token, user };
         },
-        login: async () => {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
         }
     }
 
