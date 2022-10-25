17a0e6ed516bd28456a97c4cefb6a0b110bcb013
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-10-24 21:29:44 -0700

    cleaning

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
+  //USEQUERY HOOK TO MAKE QUERY REQUEST
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  //GET THE THOUGHT OUT OF THE QUERY
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
